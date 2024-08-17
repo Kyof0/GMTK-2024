c07b8d1920e7a879b09adc05389f667f36eca644
Mehmed Ozen <102741535+mehmedozen@users.noreply.github.com>
2024-08-17 13:42:12 +0300

    Initialized state machine system

---
+++ Assets/Scripts/Data/StateData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "StateData", menuName = "Data/State Data")]
+    public class StateData : ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Data;
+using StateMachine;
+using UnityEngine;
+
+namespace Entities
+{
+    public class Entity : MonoBehaviour
+    {
+        #region Parameters
+
+        public EntityData Data;
+
+        public FiniteStateMachine StateMachine;
+
+        #endregion
+
+        #region Unity Callback Functions
+
+        protected virtual void Awake()
+        {
+            StateMachine = new FiniteStateMachine();
+        }
+
+        protected virtual void Start() { }
+
+        protected virtual void Update()
+        {
+            StateMachine.CurrentState.LogicUpdate();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            StateMachine.CurrentState.PhysicsUpdate();
+        }
+
+        protected virtual void OnDrawGizmos() { }
+
+        protected virtual void OnDestroy() { }
+
+        #endregion
+    }
+}
