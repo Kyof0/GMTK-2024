2dfc3edf281228275f06f9d310c5f3eff4e72db8
Mehmed Ozen <102741535+mehmedozen@users.noreply.github.com>
2024-08-19 19:13:10 +0300

    Added animation system

---
+++ Assets/Scripts/Animations/Atsm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animations
+{
+    public class Atsm : MonoBehaviour
+    {
+        private Rigidbody2D Rb;
+
+        private Animator Anim;
+
+        private void Awake()
+        {
+            Rb = transform.GetComponent<Rigidbody2D>();
+
+            Anim = transform.GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            Anim.SetBool("move", Rb.velocity != Vector2.zero);
+        }
+    }
+}
