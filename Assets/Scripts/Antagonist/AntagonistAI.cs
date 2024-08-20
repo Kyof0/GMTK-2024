4168f86b8cad9904cc64a79125c25c1fff9f62fe
FadenK <JamesCalvinMeadersIII@gmail.com>
2024-08-20 12:19:18 -0400

    NewAntagonist
    Needs an instance of TribeResources and TribePopulation

--- Assets/Scripts/Antagonist/AntagonistAI.cs
+++ Assets/Scripts/Antagonist/AntagonistAI.cs
@@ -1,150 +1,150 @@
-using Entities.People;
-using OldSystem.Entities;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+//using Entities.People;
+//using OldSystem.Entities;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-public abstract class AntagonistAI : MonoBehaviour {
+//public abstract class AntagonistAI : MonoBehaviour {
 
-  public enum Resource { Wood, Stone, Iron, Gold, Food};
+//  public enum Resource { Wood, Stone, Iron, Gold, Food};
 
-  // Can change this resources class to be a dictionary<Resource, float>
-  public class Resources
-  {
-    public float Wood { get; set; }
-    public float Stone { get; set; }
-    public float Iron { get; set; }
-    public float Gold { get; set; }
-    public float Food { get; set; }
+//  // Can change this resources class to be a dictionary<Resource, float>
+//  public class Resources
+//  {
+//    public float Wood { get; set; }
+//    public float Stone { get; set; }
+//    public float Iron { get; set; }
+//    public float Gold { get; set; }
+//    public float Food { get; set; }
 
-    public Resources()
-    {
-      Wood = 0;
-      Stone = 0;
-      Iron = 0;
-      Gold = 0;
-      Food = 0;
-    }
+//    public Resources()
+//    {
+//      Wood = 0;
+//      Stone = 0;
+//      Iron = 0;
+//      Gold = 0;
+//      Food = 0;
+//    }
 
-    public Resources(float wood, float stone, float iron, float gold, float food)
-    {
-      Wood = wood;
-      Stone = stone;
-      Iron = iron;
-      Gold = gold;
-      Food = food;
-    }
+//    public Resources(float wood, float stone, float iron, float gold, float food)
+//    {
+//      Wood = wood;
+//      Stone = stone;
+//      Iron = iron;
+//      Gold = gold;
+//      Food = food;
+//    }
 
-    public static bool operator >(Resources left, Resources right)
-    {
-      return left.Wood > right.Wood && left.Stone > right.Stone && left.Iron > right.Iron && left.Gold > right.Gold && left.Food > right.Food;
-    }
+//    public static bool operator >(Resources left, Resources right)
+//    {
+//      return left.Wood > right.Wood && left.Stone > right.Stone && left.Iron > right.Iron && left.Gold > right.Gold && left.Food > right.Food;
+//    }
 
-    public static bool operator <(Resources left, Resources right)
-    {
-      return left.Wood < right.Wood && left.Stone < right.Stone && left.Iron < right.Iron && left.Gold < right.Gold && left.Food < right.Food;
-    }
+//    public static bool operator <(Resources left, Resources right)
+//    {
+//      return left.Wood < right.Wood && left.Stone < right.Stone && left.Iron < right.Iron && left.Gold < right.Gold && left.Food < right.Food;
+//    }
 
-    public static Resources operator -(Resources left, Resources right)
-    {
-      return new Resources
-      { 
-      Wood = left.Wood - right.Wood,
-      Stone = left.Stone - right.Stone,
-      Iron = left.Iron - right.Iron,
-      Gold = left.Gold - right.Gold,
-      Food = left.Food - right.Food,
-      };
-    }
+//    public static Resources operator -(Resources left, Resources right)
+//    {
+//      return new Resources
+//      { 
+//      Wood = left.Wood - right.Wood,
+//      Stone = left.Stone - right.Stone,
+//      Iron = left.Iron - right.Iron,
+//      Gold = left.Gold - right.Gold,
+//      Food = left.Food - right.Food,
+//      };
+//    }
 
-    public static Resources operator +(Resources left, Resources right)
-    {
-      return new Resources
-      {
-        Wood = left.Wood + right.Wood,
-        Stone = left.Stone + right.Stone,
-        Iron = left.Iron + right.Iron,
-        Gold = left.Gold + right.Gold,
-        Food = left.Food + right.Food,
-      };
-    }
+//    public static Resources operator +(Resources left, Resources right)
+//    {
+//      return new Resources
+//      {
+//        Wood = left.Wood + right.Wood,
+//        Stone = left.Stone + right.Stone,
+//        Iron = left.Iron + right.Iron,
+//        Gold = left.Gold + right.Gold,
+//        Food = left.Food + right.Food,
+//      };
+//    }
 
-  }
+//  }
 
-  public Resources resources = new Resources();
+//  public Resources resources = new Resources();
 
-  public enum MinionType { Miner, Farmer, Soldier, LumberJack };
-  // The list should be in the same order as the enum
-  public List<GameObject> Minions;
+//  public enum MinionType { Miner, Farmer, Soldier, LumberJack };
+//  // The list should be in the same order as the enum
+//  public List<GameObject> Minions;
 
-  public MinionType GetMinionType(Resource resource) 
-  {
-    switch (resource)
-    {
-      case Resource.Wood: return MinionType.LumberJack;
-      case Resource.Stone: return MinionType.Miner;
-      case Resource.Iron: return MinionType.Miner;
-      case Resource.Gold: return MinionType.Miner;
-      case Resource.Food: return MinionType.Farmer;
-      default:
-        Debug.LogError("Unknown resource");
-        return MinionType.Soldier;
-    }
+//  //public MinionType GetMinionType(Resource resource) 
+//  //{
+//  //  switch (resource)
+//  //  {
+//  //    case Resource.Wood: return MinionType.LumberJack;
+//  //    case Resource.Stone: return MinionType.Miner;
+//  //    case Resource.Iron: return MinionType.Miner;
+//  //    case Resource.Gold: return MinionType.Miner;
+//  //    case Resource.Food: return MinionType.Farmer;
+//  //    default:
+//  //      Debug.LogError("Unknown resource");
+//  //      return MinionType.Soldier;
+//  //  }
    
-  }
+//  //}
 
 
-  // Start is called before the first frame update
-  void Start() {
+//  // Start is called before the first frame update
+//  void Start() {
 
-  }
+//  }
 
-  // Update is called once per frame
-  void Update() {
+//  // Update is called once per frame
+//  void Update() {
 
-  }
+//  }
 
-  protected void PurchaseMinion(MinionType minion)
-  {
-    var cost = GetMinionPrice(minion);
-    if (cost > resources) {
-      Debug.LogError("Attempted to purchase an unaffordable minion. Use \"CanPurchaseMinion()\" to determine");
-      return;
-    }
+//  protected void PurchaseMinion(MinionType minion)
+//  {
+//    var cost = GetMinionPrice(minion);
+//    if (cost > resources) {
+//      Debug.LogError("Attempted to purchase an unaffordable minion. Use \"CanPurchaseMinion()\" to determine");
+//      return;
+//    }
 
-    resources -= cost;
-    SpawnMinion(minion);
-  }
+//    resources -= cost;
+//    SpawnMinion(minion);
+//  }
 
 
-  // Wood, Stone, Iron, Gold, Food
-  protected Resources GetMinionPrice(MinionType minion)
-  {
-    // Implement minion costs
-    return new Resources();
-  }
+//  // Wood, Stone, Iron, Gold, Food
+//  protected Resources GetMinionPrice(MinionType minion)
+//  {
+//    // Implement minion costs
+//    return new Resources();
+//  }
 
-  private void SpawnMinion(MinionType minion)
-  {
-        InstantiateMinion(minion);
-  }
+//  private void SpawnMinion(MinionType minion)
+//  {
+//        InstantiateMinion(minion);
+//  }
 
-  private void InstantiateMinion(MinionType minionType)
-  {
-    if(Minions.Count > (int)minionType)
-    {
-      var minion = Minions[(int)minionType];
-      Instantiate(minion, transform.position, Quaternion.identity);
-    } 
-    else
-    {
-      Debug.Log("Attempted to spawn an unassigned minion");
-    }
-    // var instance = Instantiate(minion, transform.position, Quaternion.identity);
-    // var minionScript = instance.GetComponent<Collector>();
-    // minionScript.Targets = this.targets;
-    // minionScript.GameManager = this;
-  }
+//  private void InstantiateMinion(MinionType minionType)
+//  {
+//    if(Minions.Count > (int)minionType)
+//    {
+//      var minion = Minions[(int)minionType];
+//      Instantiate(minion, transform.position, Quaternion.identity);
+//    } 
+//    else
+//    {
+//      Debug.Log("Attempted to spawn an unassigned minion");
+//    }
+//    // var instance = Instantiate(minion, transform.position, Quaternion.identity);
+//    // var minionScript = instance.GetComponent<Collector>();
+//    // minionScript.Targets = this.targets;
+//    // minionScript.GameManager = this;
+//  }
 
-}
+//}
--- Assets/Scripts/Antagonist/BalancedMinions.cs
+++ Assets/Scripts/Antagonist/BalancedMinions.cs
@@ -1,52 +1,52 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class BalancedAntagonist : AntagonistAI
-{
+//public class BalancedAntagonist : AntagonistAI
+//{
 
-  private MinionType nextMinion = 0;
-  private int minionTypeCount = Enum.GetValues(typeof(MinionType)).Length;
+//  private MinionType nextMinion = 0;
+//  private int minionTypeCount = Enum.GetValues(typeof(MinionType)).Length;
 
-  public float SpawnInterval = 2.0f;
-  private float lastSpawnTime; 
+//  public float SpawnInterval = 2.0f;
+//  private float lastSpawnTime; 
 
-  // Start is called before the first frame update
-  void Start()
-  {
+//  // Start is called before the first frame update
+//  void Start()
+//  {
 
-  }
+//  }
 
-  // Update is called once per frame
-  void Update()
-  {
-    bool canSpawn = GetCanSpawn();
-    if (canSpawn) { 
-      SpawnNextMinion(); 
-      lastSpawnTime = Time.time;
-    }
-  }
+//  // Update is called once per frame
+//  void Update()
+//  {
+//    bool canSpawn = GetCanSpawn();
+//    if (canSpawn) { 
+//      SpawnNextMinion(); 
+//      lastSpawnTime = Time.time;
+//    }
+//  }
 
-  bool GetCanSpawn()
-  {
-    if (resources < GetMinionPrice(nextMinion)) { return false; }
+//  bool GetCanSpawn()
+//  {
+//    if (resources < GetMinionPrice(nextMinion)) { return false; }
 
-    if ((Time.time - lastSpawnTime) < SpawnInterval) { return false; }
+//    if ((Time.time - lastSpawnTime) < SpawnInterval) { return false; }
 
-    return true;
-  }
+//    return true;
+//  }
 
-  void SpawnNextMinion()
-  {
-    PurchaseMinion(nextMinion);
-    IncrementNextMinion();
-  }
+//  void SpawnNextMinion()
+//  {
+//    PurchaseMinion(nextMinion);
+//    IncrementNextMinion();
+//  }
 
-  void IncrementNextMinion()
-  {
-    int nextMinionIndex = (((int)nextMinion) + 1) % minionTypeCount;
-    nextMinion = (MinionType)nextMinionIndex;
-    // Debug.Log(nextMinion);
-  }
-}
+//  void IncrementNextMinion()
+//  {
+//    int nextMinionIndex = (((int)nextMinion) + 1) % minionTypeCount;
+//    nextMinion = (MinionType)nextMinionIndex;
+//    // Debug.Log(nextMinion);
+//  }
+//}
