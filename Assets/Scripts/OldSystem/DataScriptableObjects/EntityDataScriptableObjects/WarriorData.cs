9da21a9fdfa21aaba3d6f1ae3ccd838c277c8a04
Mehmed Ozen <102741535+mehmedozen@users.noreply.github.com>
2024-08-20 14:58:45 +0300

    shepherd and warrior complete

---
+++ Assets/Scripts/OldSystem/DataScriptableObjects/EntityDataScriptableObjects/WarriorData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace OldSystem.DataScriptableObjects.EntityDataScriptableObjects
+{
+    [CreateAssetMenu(fileName = "EntityData", menuName = "Data/Entity Data/Warrior Data")]
+    public class WarriorData : EntityData
+    {
+        public float moveSpeed;
+        public float killTime;
+        public float wanderPeriod;
+    }
+}
